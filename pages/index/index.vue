15873de44d0395058b2955681919dc9903db2fa0
huoweiguo <huoweiguo123>
2020-03-12 21:57:17 +0800

    优化首页

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -2,53 +2,47 @@
 	<view class="content">
 		<div class="notice">uni-app 是一个使用 Vue.js 开发所有前端应用的框架,开发者编写一套代码,</div>
 		<div class="mb10"></div>
-		<uni-grid :column="3" borderColor="#f1f1f7">
-			<uni-grid-item>
+		<ul class="home-grid">
+			<li>
 				<img src="/static/images/icon-car.png" />
-				<text class="text">出车报名</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">出车报名</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-sign.png" />
-				<text class="text">今日签到</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">今日签到</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-goods.png" />
-				<text class="text">核货中心</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">核货中心</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-deliver.png" />
-				<text class="text">配送任务</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">配送任务</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-door.png" />
-				<text class="text">上门取件</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">上门取件</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-cash.png" />
-				<text class="text">财务交款</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">财务交款</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-handover.png" />
-				<text class="text">包裹交接</text>
-			</uni-grid-item>
-			<uni-grid-item>
+				<span class="text">包裹交接</span>
+			</li>
+			<li>
 				<img src="/static/images/icon-approval.png" />
-				<text class="text">我的审批</text>
-			</uni-grid-item>
-		</uni-grid>
+				<span class="text">我的审批</span>
+			</li>
+		</ul>
 	</view>
 </template>
 
 <script>
-	import { uniGrid, uniGridItem } from '@dcloudio/uni-ui'
 	export default {
-		components: {
-			uniGrid, uniGridItem
-		}, 
 		data() {
-			return {
-				title: 'Hello'
-			}
+			return {}
 		},
 		onLoad() {
 
@@ -75,20 +69,27 @@
 		background-position: 10px center;
 		background-size: 20px auto;
 	}
-	.uni-grid-item {
-		display: flex;
+	.home-grid {
+		overflow: hidden;
 		background-color: #fff;
-		flex-direction: column;
-		justify-content: center;
-		text-align: center;
-		img {
-			width: 30px;
-			display: block;
-			margin:30% auto 10px;
-		}
-		text {
-			font-size: 16px;
-			color: #777;
+		li {
+			float: left;
+			width: 33.33%;
+			text-align: center;
+			height: 120px;
+			padding-top: 30px; 
+			border-right: 1px solid #f1f1f1;
+			border-bottom: 1px solid #f1f1f1;
+			img {
+				width: 30px;
+				display: block;
+				margin:0 auto 10px;
+			}
+			.text {
+				display: block;
+				font-size: 16px;
+				color: #777;
+			}
 		}
 	}
 </style>
